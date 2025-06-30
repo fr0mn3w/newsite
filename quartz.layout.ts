4f3b8cde819b881660e8e37436b62d69592865e4
t0mmytr1nh <tommytrinh1@gmail.com>
2025-06-30 17:28:58 -0400

    Quartz sync: Jun 30, 2025, 5:28 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,7 +6,11 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  },
+  footer: Component.Footer({
+    links: {
+    },
+  }),
+}
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {

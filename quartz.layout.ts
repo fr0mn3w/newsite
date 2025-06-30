3933e7df56040e4e7a4a181ddf1a6fbbd934c01f
t0mmytr1nh <tommytrinh1@gmail.com>
2025-06-30 17:49:53 -0400

    Quartz sync: Jun 30, 2025, 5:49 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,6 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
+
     },
   }),
 }
@@ -47,7 +48,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),

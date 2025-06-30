ecc47422cffc1b3bc600fcfd00e2fc7941d6ab28
t0mmytr1nh <tommytrinh1@gmail.com>
2025-06-30 17:26:38 -0400

    Quartz sync: Jun 30, 2025, 5:26 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,11 +6,6 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
   }),
 }
 
